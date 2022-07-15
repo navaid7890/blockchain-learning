b48a436d8f8adec35994e47802bcc5216f9f5b38
Navaid <muhammad.navaid@invixible.com>
2022-07-15 12:10:00 +0500

    Added Comments
    - Added method level comments

--- blockchain.py
+++ blockchain.py
@@ -3,11 +3,17 @@ import json as _json
 import hashlib as _hashlib
 
 class Blockchain:
+    '''
+    class constructor, contains "List", "Block" and append the list with block
+    '''
     def __init__(self) -> None:
         self.chain = list()
         genesis_block = self._create_block(data="I'm the genesis block", proof=1, previous_hash="0", index=1)
         self.chain.append(genesis_block)
 
+    '''
+    Function to create a block with required fields
+    '''
     def _create_block(self, data:str, proof: int, previous_hash: str, index: int, ) -> dict:
         block = {
             "index": index,
